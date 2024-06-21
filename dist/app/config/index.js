10a57571523ef5d89acbb48e629d5b2e0f4a7011
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-06-21 18:09:28 +0600

    added Redme file

--- dist/app/config/index.js
+++ dist/app/config/index.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-dotenv_1.default.config({ path: path_1.default.join(process.cwd(), "env") });
+dotenv_1.default.config({ path: path_1.default.join((process.cwd(), '.env')) });
 exports.default = {
+    NODE_ENV: process.env.NODE_ENV,
     port: process.env.PORT,
-    db_url: process.env.DATABASE_URL,
-    // secret:process.env.SECRET,
-    // salt:process.env.SALT,
-    // jwt:process.env.JWT,
-    // cors:process.env.CORS,
+    database_url: process.env.DATABASE_URL,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    default_password: process.env.DEFAULT_PASS,
+    jwt_access_secret: process.env.ACCESS_JWT_SECRET,
+    Jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
 };
